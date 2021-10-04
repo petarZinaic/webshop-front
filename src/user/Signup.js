826b23e16115f0562edf1e9c5228ad0cdf938e3f
petar-zinaic <petar.zinaic@marbleit.rs>
2021-10-04 22:26:04 +0200

    Setup state for signup

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,10 +1,53 @@
+import { useState } from "react";
 import Layout from "../core/Layout";
 import { API } from "../config";
 
 const Signup = () => {
+
+    const [ values, setValues ] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        sucess: false
+    });
+
+    const handeChange = name => event => {
+        setValues({...values, error: false, [name]: event.target.value})
+    }
+
+    const signUpForm = () => {
+        return (
+            <form>
+                <div className="form-group">
+                    <label className="text-muted"> Name</label>
+                    <input onChange={handeChange("name")} type="text" className="form-control"></input>
+                </div>
+
+                <div className="form-group">
+                    <label className="text-muted"> Email</label>
+                    <input onChange={handeChange("email")} type="email" className="form-control"></input>
+                </div>
+
+                <div className="form-group">
+                    <label className="text-muted"> Password</label>
+                    <input onChange={handeChange("password")} type="password" className="form-control"></input>
+                </div>
+                <button className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
+        )
+    }
+
     return(
-        <Layout title="Signup" description="Signup to React E-commperce App">
-        {API}
+        <Layout 
+            title="Signup"
+            description="Signup to React E-commperce App"
+            className="container col-md-8 offset-md-2"
+            >
+           {signUpForm()}
+           {JSON.stringify(values)}
         </Layout>
     )
 }
