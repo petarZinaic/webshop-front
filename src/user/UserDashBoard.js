46958b4a11192bf7065f00e5a569233da7891f22
petar-zinaic <petar.zinaic@marbleit.rs>
2021-10-07 23:48:06 +0200

    Links on user dashboard

--- src/user/UserDashBoard.js
+++ src/user/UserDashBoard.js
@@ -1,12 +1,29 @@
 import { isAuthenticated } from "../auth";
 import Layout from "../core/Layout";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
 
-    const { user: {_id, name, email, role} } = isAuthenticated()
+    const { user: {_id, name, email, role} } = isAuthenticated();
 
-    return (
-        <Layout title="Dashboard" description="User Dashboard" className="container">
+    const userLinks = () => {
+        return (
+            <div className="card">
+                <h4 className="card-header">User Links</h4>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <Link className="nav-link" to="/cart">My Cart</Link>
+                    </li>
+                    <li className="list-group-item">
+                        <Link className="nav-link" to="/profile/update">Update Profile</Link>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+
+    const userInfo = () => {
+        return (
             <div className="card mb-5">
                 <h3 className="card-header">User Information</h3>
                 <ul className="list-group">
@@ -15,13 +32,31 @@ const Dashboard = () => {
                     <li className="list-group-item">{role === 1 ? "Admin" : "Registered User"}</li>
                 </ul>
             </div>
-                
-                <div className="card mb-5">
-                    <h3 className="card-header">Purchase history</h3>
-                    <ul className="list-group">
-                        <li className="list-group-item">history</li>
-                    </ul>
-                </div>
+            )
+    }
+
+    const purchaseHistory = () => {
+        return (
+            <div className="card mb-5">
+                <h3 className="card-header">Purchase history</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">history</li>
+                </ul>
+            </div>
+        )
+    }
+
+    return (
+        <Layout title="Dashboard" description={`G day ${name}!`} className="container-fluid">
+         <div className="row">
+           <div className="col-3">
+               {userLinks()}
+            </div>
+            <div className="col-9">
+               {userInfo()} 
+               {purchaseHistory()}
+            </div>
+         </div>
         </Layout>
     )
 }
