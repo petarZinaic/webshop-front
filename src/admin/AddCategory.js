5938b46408762a9d1e3119b6f9b2e4c42146b12b
petar-zinaic <petar.zinaic@marbleit.rs>
2021-10-09 19:16:14 +0200

    Add products

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -81,7 +81,7 @@ const AddCategory = () => {
             description={`G day ${user.name}, ready to add a new category`}
         >
         <div className="row">
-          <div className="col-8 offset-md-2">
+          <div className="col-md-8 offset-md-2">
               {showSuccess()}
               {showError()}
               {newCategoryForm()}
