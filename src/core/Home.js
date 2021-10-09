0296b7b6bf51158a40969eaed14aa1a221ed16c0
petar-zinaic <petar.zinaic@marbleit.rs>
2021-10-09 19:31:22 +0200

    Products by arrival and sell

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,9 +1,43 @@
+import { useState ,useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
 
 const Home = () => {
+
+    const [ productsBySell, setProductsBySell ] = useState([]);
+    const [ productsByArrival, setProductsByArrival ] = useState([]);
+    const [ error, setError ] = useState(false);
+
+    const loadProductsBySell = () => {
+        getProducts("sold").then(data => {
+            if(data.error) {
+                setError(data.error)
+            } else { 
+                setProductsBySell(data);
+            }
+        });
+    }
+
+    const loadProductsByArrival = () => {
+        getProducts("createdAt").then(data => {
+            if(data.error) {
+                setError(data.error)
+            } else { 
+                setProductsByArrival(data);
+            }
+        });
+    }
+
+    useEffect(() => {
+        loadProductsByArrival()
+        loadProductsBySell()
+    }, [])
+
     return (
        <Layout title="Home Page" description="Node React E-commperce App">
-           ...
+           {JSON.stringify(productsByArrival)}
+           <hr />
+           {JSON.stringify(productsBySell)}
         </Layout>
     )
 }
