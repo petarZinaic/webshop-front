e78b429a49b7c45c844ead4fceea77435f33277f
petar-zinaic <petar.zinaic@marbleit.rs>
2021-10-10 16:24:48 +0200

    Handle categories toggle

--- src/core/Checkbox.js
+++ src/core/Checkbox.js
@@ -2,9 +2,27 @@ import { useState } from "react";
 
 const Checkbox = ({ categories }) => {
 
+    const [ checked, setChecked ] = useState([]);
+
+    const handleToggle = c => () => {
+        const currentCategoryId = checked.indexOf(c);
+        const newCheckedCategoryId = [...checked];
+
+        if(currentCategoryId === -1) {
+            newCheckedCategoryId.push(c)
+        } else {
+            newCheckedCategoryId.splice(currentCategoryId, 1);
+        }
+
+        console.log(newCheckedCategoryId);
+        setChecked(newCheckedCategoryId);
+    }
+
     return categories.map((c, i) => (
         <li key={i} className="list-unstyled">
             <input
+                value={checked.indexOf(c._id === -1)}
+                onChange={handleToggle(c._id)}
                 type="checkbox"
                 className="form-check-input"
             />
