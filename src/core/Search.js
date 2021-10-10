6003fac16160e59a600ec1b9aea8e3465d032dd0
petar-zinaic <petar.zinaic@marbleit.rs>
2021-10-10 20:13:44 +0200

    Search bar

---
+++ src/core/Search.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import { getCategories, getProducts } from "./apiCore";
+import Card from "./Card";
+
+const Search = () => {
+    const [ data, setData ] = useState({
+        categories: [],
+        category: "",
+        search: "",
+        results: [],
+        searched: false
+    });
+
+    const { categories, category, search, results, searched } = data;
+
+    const loadCategories = () => {
+        getCategories().then(data => {
+            if(data.error) {
+                console.log(data.error);
+            } else {
+                setData({...data, categories: data});
+            }
+            
+        });
+    }
+ 
+    useEffect(() => {
+        loadCategories()
+    }, [])
+
+    const searchSubmit = () => {
+
+    }
+
+    const handleChange = () => {
+
+    }
+
+
+
+    const searchForm = () => (
+        <form onSubmit={searchSubmit}>
+           <span className="input-group-text">
+                <div className="input-group input-group-lg">
+                    <div className="input-group-prepend"> 
+                        <select className="btn mr-2" onChange={handleChange("category")}>
+                            <option value="All">Pick Category</option>
+                            {categories.map((c, i) => (<option key={i} value={c._id}>{c.name}</option>))}
+                        </select>
+                    </div>
+                    <input
+                        type="search"
+                        className="form-control"
+                        onChange={handleChange("search")}
+                        placeholder="Search by name"
+                    />    
+                </div>
+                <div className="btn input-group-append" style={{ border: "none" }}>
+                    <button className="inout-group-text">Search</button>
+                </div>
+           </span>
+        </form>
+    )
+
+    return (
+        <div className="row">
+           <div className="container">
+            {searchForm()}
+           </div>
+        </div>
+    )
+}
+
+export default Search;
+
