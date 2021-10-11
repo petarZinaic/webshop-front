b7fbce19598fdcf078d4c9ec64ce3fdc6f220e73
petar-zinaic <petar.zinaic@marbleit.rs>
2021-10-11 22:08:16 +0200

    Show related products

--- src/core/Product.js
+++ src/core/Product.js
@@ -1,11 +1,12 @@
 import { useState ,useEffect } from "react";
 import Layout from "./Layout";
-import { read } from "./apiCore";
+import { read, listRelated } from "./apiCore";
 import Card from "./Card";
 
 const Product = props => {
 
     const [ product, setProduct ] = useState({})
+    const [ relatedProduct, setRelatedProduct ] = useState([])
     const [ error, setError ] = useState(false);
 
     const loadSingleProduct = productId => {
@@ -14,6 +15,13 @@ const Product = props => {
                 setError(data.error);
             } else {
                 setProduct(data);
+                listRelated(data._id).then(data => {
+                    if(data.error) {
+                        setError(data.error)
+                    } else {
+                        setRelatedProduct(data)
+                    }
+                })
             }
         })
     }
@@ -21,7 +29,7 @@ const Product = props => {
     useEffect(() => {
         const productId = props.match.params.productId;
         loadSingleProduct(productId);
-    }, [])
+    }, [props])
 
     return (
         <Layout
@@ -34,14 +42,24 @@ const Product = props => {
                 className="container-fluid"
         >
             <div className="row">
+               <div className="col-8"> 
                 {
-                    product &&
-                    product.description &&
-                    <Card
-                        product={product} 
-                        showViewProductButton={false}
-                    />
-                }
+                        product &&
+                        product.description &&
+                        <Card
+                            product={product} 
+                            showViewProductButton={false}
+                        />
+                    }
+               </div>
+               <div className="col-4">
+                    <h4>Related products</h4>
+                    {relatedProduct.map((p, i) => (
+                        <div className="mb-3"> 
+                            <Card key={i} product={p} />
+                        </div>
+                    ))}
+                </div>
             </div>
          </Layout>
      )
